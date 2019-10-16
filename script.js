384b8e9e5617eac416a67fc440d53b51412e73a8
Aleksandra <puszka004@gmail.com>
2019-10-16 21:47:23 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://desolate-chamber-86714.herokuapp.com/v1/task/getTasks';
+  var apiRoot = 'https://desolate-chamber-86714.herokuapp.com/v1/task';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
